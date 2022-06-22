11a66045219026a9c7dc73ee67ecd91772a185fa
yulie08palechor <102261562+yulie08palechor@users.noreply.github.com>
2022-06-21 19:39:40 -0500

    primer repo

---
+++ main.js
@@ -0,0 +1,52 @@
+Vue.component("tv-show-list",{
+    data:function(){
+     return{
+        tvshows:[
+            { nombre: 'game of thorones',temporadas: 1 },
+            { nombre: 'game of thorones',temporadas: 2},
+            { nombre: 'game of thorones',temporadas: 3},
+            { nombre: 'game of thorones',temporadas: 4},
+            
+        ]  
+     }   
+    },
+    template: `
+    <ul> 
+    </ul>
+    `
+})
+
+
+
+ Vue.component('tv-show',{         // estamos crando un componente llamado tv-show
+ props: {
+ nombre: String,
+ temporadas: Number,
+ },
+ template: `  
+ <li>
+ <strong> {{ nombre }}</strong> ({{ temporadas}} Temporadas)
+ </li>
+ `
+ 
+})     
+
+
+const app =new Vue({
+el:'#app',
+data:{
+    mostrar: true,
+   /*tvshows:[
+        { nombre: 'game of thorones',temporadas: 1 },
+        { nombre: 'game of thorones',temporadas: 2},
+        { nombre: 'game of thorones',temporadas: 3},
+        { nombre: 'game of thorones',temporadas: 4},
+        
+    ]*/
+},
+methods:{
+    changeVisibility: function(){
+        this.mostrar=!this.mostrar
+    }
+}
+})
